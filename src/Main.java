1f7f45a9f90ae07054db01931a5208362da028c3
tuyaarak <kobrat-22@mail.ru>
2022-12-28 20:59:57 +0900

    Lesson4 т 28.12.22

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,38 @@
 import java.util.Scanner;
 
 public class Main {
+
+    public static void function1() {
+        System.out.println("exit");
+        //function2();
+
+
+    }
+
+    public static String function2() {
+        return "exit";
+    }
+
     public static void main(String[] args) {
 
-       // оператор ветвления break
-       Scanner scanner = new Scanner(System.in);
+
+        // оператор ветвления break
+        Scanner scanner = new Scanner(System.in);
         int input = scanner.nextInt();
         int i = 0;
         int n = 0; // счетчик
         while (i * i <= input) {
-             System.out.println(i* i);
-        if (n == input) {
-            break;
+            System.out.println(i * i);
+            if (n == 5) {
+                // обрываем цикл на условиях и перешли к функции function1()
+                function1();
+                break;
+            }
+            n++; // счетчик
+            i++;
         }
-        n++; // счетчик
-        i++;
-      }
-    /*
-      int u = 0;
-    while (u < 10) {
-        if (u == 5) {
-            continue;
-        }
-        System.out.println(i* i);
-        }*/
+        // инициировали переменную secondFunction функцией function2()  вывели на консоль
+        String secondFunction = function2();
+        System.out.println(secondFunction);
     }
 }
